cf3ee6632d8668315309ecb7f84f465bef7d2403
Yogesh S <firzenyogesh@gmail.com>
2020-07-27 15:21:28 +0530

    zsh-kit

--- zsh-kit.sh
+++ zsh-kit.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+MODE="install"
+
+if [[ -z "$1" ]]; then
+    if [ $1 == "uninstall" || $1 == "remove" || $1 == "purge" ]; then
+        MODE="uninstall";
+    fi
+fi
+
+if [[$MODE == "install"]]; then
+    if [[ -e "$HOME/.oh-my-zsh" ]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    else
+        upgrade_oh_my_zsh
+    fi
+else
+    uninstall_oh_my_zsh
+fi
