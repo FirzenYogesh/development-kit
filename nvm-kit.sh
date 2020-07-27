162069c34cbc65e38333a6272430155fe3e8c63f
Yogesh S <firzenyogesh@gmail.com>
2020-07-27 15:14:32 +0530

    fixes

--- nvm-kit.sh
+++ nvm-kit.sh
@@ -6,7 +6,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install
 
 echo '# nvm PATH Setup
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm" >> "$SHELL_DEPENDENCIES/paths'
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> "$SHELL_DEPENDENCIES/paths"
 
 source "$SHELL_DEPENDENCIES/main"
 
