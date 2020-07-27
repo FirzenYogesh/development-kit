e9104763bd7d8251130846f4c4e5067f378b62ec
Yogesh S <firzenyogesh@gmail.com>
2020-07-27 13:05:55 +0530

    docker-kit

--- development-kit.sh
+++ development-kit.sh
@@ -4,7 +4,7 @@
 #sudo apt update
 
 # install basic dependencies
-#sudo apt install unzip zip curl git xz-utils
+#sudo apt install -y unzip zip curl git xz-utils
 
 # tools to install
 askWhichTool() {
@@ -15,13 +15,24 @@ docker
 code-server
 "
 
-    local tool="zsh"
-    read -rp "Type ahead with your choice: " -e -i "$tool" tool
+    local tool=""
+    read -e -rp "Type ahead with your choice: " tool
+    installTool $tool
 }
 
 installTool() {
     local tool=$1
     echo "Installing $tool"
+    if [[ $tool == "zsh"]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    elif [[ $tool == "nvm"]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh)"
+        nvm install
+        npm i -g typescript
+    elif [[ $tool == "docker"]]; then
+        
+
+    fi
 }
 
 if [[ $1 == "install" ]]; then
---
+++ docker-kit.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Check OS Version
+
+# Debian or Ubuntu
+MODE="install"
+
+if [[ -z "$1" ]]; then
+    if [ $1 == "uninstall" || $1 == "remove" || $1 == "purge" ]; then
+        MODE="uninstall";
+    fi
+fi
+
+if [[ -e /etc/debian_version ]]; then
+    source /etc/os-release
+    OS=$ID
+elif [[ -e /etc/fedora-release ]]; then
+    source /etc/os-release
+	OS=$ID
+elif [[ -e /etc/fedora-release ]]; then
+	source /etc/os-release
+	OS=$ID
+elif [[ -e /etc/centos-release ]]; then
+	OS=centos
+elif [[ -e /etc/arch-release ]]; then
+	OS=arch
+    echo "Currently the script does not support Arch Linux"
+    exit 1
+fi
+
+if [[ $OS == "ubuntu"]]; then
+    sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io
+elif [[ $OS == "debian" ]]; then
+    sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io
+elif [[ $OS == "fedora" ]]; then
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf install docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+elif [[ $OS == "centos" ]]; then
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+fi
+
+sudo docker run hello-world
